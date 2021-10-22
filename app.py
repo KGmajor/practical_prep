2efe4e22a60834340c210d4eb6e5b480330ae5e4
Kristal <kristal@joinyearone.io>
2021-10-22 13:54:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,38 @@ def get_time():
 
 
 #TODO Add a route that displays the current time for all US timezones.
+@app.route('/time/all', methods=['GET'])
+def get_all_time():
+    """
+    The endpoint takes in optional `tz`
+    argument to specify a timezone, returning
+    current time in the HH:MM:SS format.
 
+    Example:
+        /time?tz=America/Los+Angeles
+    Returns:
+        {"America/Los_Angeles": "11:16:28"}
+    """
+    def _get_local_time():
+        local_time = datetime.datetime.now()
+        time_string = local_time.strftime("%H:%M:%S")
+        return time_string
+
+
+    if request.args:
+        tz = request.args.get('tz')
+        try:
+            tz = tz.replace(" ", "_")
+        except Exception:
+            return f"Could not process timezone: {tz}", 404
+        try:
+            local_time = datetime.datetime.now()
+            tz_time = pytz.timezone(tz)
+            tz_localized = local_time.astimezone(tz_time)
+            time_string = tz_localized.strftime("%H:%M:%S")
+            return json.dumps({f"{tz}": time_string})
+        except Exception:
+            return f"Could not process timezone", 404
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')
